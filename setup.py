9ba1c42850cbf1df1f51fe78fb08e5dbd4dd8a94
alokpandey098 <kalok0575@gmail.com>
2024-09-26 15:42:45 +0530

    Requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="USvisa",
+    version="0.0.0",
+    author="Alok",
+    author_email="kalok0575@gmail.com",
+    packages=find_packages()
+)
