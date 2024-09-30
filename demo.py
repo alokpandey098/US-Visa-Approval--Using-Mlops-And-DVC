5bdf77826abffa37c6c696043d0501058e7ab100
alokpandey098 <kalok0575@gmail.com>
2024-09-30 14:48:29 +0530

    Exception done

--- demo.py
+++ demo.py
@@ -1,3 +1,10 @@
 from USvisa.logger import logging
+from USvisa.exception import USvisaException
+import sys
 
 logging.info("Testing Custom logs ...")
+
+try:
+    a = 2/0
+except Exception as e:
+    raise USvisaException(e,sys)
