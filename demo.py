0e2f59c26bf23618cce46bff6be9019b9b1a0bc9
alokpandey098 <kalok0575@gmail.com>
2024-10-02 15:29:04 +0530

    Data Ingestion

--- demo.py
+++ demo.py
@@ -1,10 +1,3 @@
-from USvisa.logger import logging
-from USvisa.exception import USvisaException
-import sys
-
-logging.info("Testing Custom logs ...")
-
-try:
-    a = 2/0
-except Exception as e:
-    raise USvisaException(e,sys)
+import os
+url = os.getenv('MONGODB_URL')
+print(url)
