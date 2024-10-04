8b5d3ef2bcb5044aff2e66468cda84f825eb0bfb
alokpandey098 <kalok0575@gmail.com>
2024-10-04 15:52:28 +0530

    Model Evaluation and Model pusher asw s3

--- demo.py
+++ demo.py
@@ -1,9 +1,8 @@
+# import os
+
+# url = os.getenv("MONGODB_URL")
+# print(url)
 from USvisa.pipline.training_pipeline import TrainPipeline
 
-# Instantiate the pipeline class and run it
 obj = TrainPipeline()
 obj.run_pipeline()
-
-# import os
-# url = os.getenv('MONGODB_URL')
-# print(url)
